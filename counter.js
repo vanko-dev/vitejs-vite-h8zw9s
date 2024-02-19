78e9f20f039036e4928b9b476786cdff21dc55a2
vanko-dev <ivan.vaskovskiy@derivco.co.uk>
2024-02-19 20:11:42 +0000

    intervals

--- counter.js
+++ counter.js
@@ -3,13 +3,19 @@ import debounce from 'lodash.debounce';
 export function setupTimer(element) {
   element.innerHTML = '';
 
-  const setCounter = () => {
-    element.innerHTML = 'AAA';
+  const setCounter = (start) => {
+
+    return () => {
+      element.innerHTML += `${new Date() - start}<br>`;
+      setTimeout(setCounter(new Date()), 1 * 1000);
+    }
+
+    
   };
 
   //element.addEventListener('click', () => setCounter(counter + 1));
-  setTimeout(setCounter, 20 * 1000);
+  setTimeout(setCounter(new Date()), 1 * 1000);
 
   // const a = debounce(setCounter, 20 * 1000);
-  a('111');
+  //a('111');
 }
