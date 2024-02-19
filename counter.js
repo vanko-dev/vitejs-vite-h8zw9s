0b3c3d0b89f5543ff0022e81c3d49a38e8c9be15
vanko-dev <ivan.vaskovskiy@derivco.co.uk>
2024-02-19 20:32:29 +0000

    wait

--- counter.js
+++ counter.js
@@ -12,9 +12,43 @@ export function setupTimer(element) {
     
   };
 
+  waitForUserInactivityImpl(setCounter, 20 * 1000)
+
   //element.addEventListener('click', () => setCounter(counter + 1));
-  setTimeout(setCounter(new Date()), 20 * 1000);
+  // setTimeout(setCounter(new Date()), 20 * 1000);
 
   // const a = debounce(setCounter, 20 * 1000);
   //a('111');
+}
+
+
+function waitForUserInactivityImpl(callback, ms) {
+  let reportActivity;
+
+  const activate = () => {
+    reportActivity = debounce(callback, ms);
+    reportActivity("start");
+
+    document.addEventListener("mousemove", reportActivity, true);
+    document.addEventListener("touchstart", reportActivity, true);
+    document.addEventListener("keydown", reportActivity, true);
+    // This custom event listener has been added in to monitor when a user does a spin on a casino game
+    document.addEventListener("spinevent", reportActivity, true);
+  };
+
+  const deactivate = () => {
+    reportActivity.cancel();
+    document.removeEventListener("mousemove", reportActivity, true);
+    document.removeEventListener("touchstart", reportActivity, true);
+    document.removeEventListener("keydown", reportActivity, true);
+    document.removeEventListener("spinevent", reportActivity, true);
+  };
+
+  activate();
+
+  return {
+    cancel: deactivate,
+    pause: deactivate,
+    resume: activate,
+  };
 }
