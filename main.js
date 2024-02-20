a83f030cf70a148ac7905cd3834ee8d95673ad83
vanko-dev <ivan.vaskovskiy@derivco.co.uk>
2024-02-20 19:26:58 +0000

    f

--- main.js
+++ main.js
@@ -4,17 +4,11 @@ import { startTimer } from './startTimers.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
     <div class="card">
       <button id="new" type="button"></button>
       <button id="old" type="button"></button>
       <button id="timer" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `;
 
