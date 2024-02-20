d1650ee0ba340d12cf2b79dd0c961aa4d24afe87
vanko-dev <ivan.vaskovskiy@derivco.co.uk>
2024-02-20 22:35:32 +0000

    f

--- waitForUserInactivityImplNew.js
+++ waitForUserInactivityImplNew.js
@@ -2,16 +2,16 @@ import { createUserActivityDetector } from "./createUserActivityDetector"
 
 export const waitForUserInactivityImpl = (oninactivity, ms) => {
   const TIMEOUT_K = 0.9;
-  const TIMEOUT_PRECISION_MS = 1 * 10;
+  const TIMEOUT_PRECISION_MS = 1000;
 
   let activityDetector = createUserActivityDetector();
   let timeout = setTimeout(checkInactivityTime, getNextCheckInMs());
 
   document.addEventListener("visibilitychange", onVisibilityChange);
-  
+
   function getNextCheckInMs() {
-    const nextInMs = (activityDetector.lastActivityTime() + ms - Date.now()) * TIMEOUT_K;
-    return Math.max(nextInMs, TIMEOUT_PRECISION_MS);
+    const nextInMs = (activityDetector.lastActivityTime() + ms - Date.now());
+    return nextInMs < TIMEOUT_PRECISION_MS ? nextInMs : nextInMs * TIMEOUT_K;
   }
 
   function checkInactivityTime() {
